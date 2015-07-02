4176cf015af3202e78077afcc948b8709853320d
Trojaner_ <trojaner25@gmail.com>
2015-07-02 22:34:43 +0300

    Initialize Commit

---
+++ RocketInstaller/InstallerForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿using MetroFramework.Controls;
+
+namespace RocketInstaller
+{
+    partial class InstallerForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+         private void InitializeComponent()
+        {
+            this.InstallButton = new MetroFramework.Controls.MetroButton();
+            this.InstallDirectory = new MetroFramework.Controls.MetroTextBox();
+            this.BrowseButton = new MetroFramework.Controls.MetroButton();
+            this.InstallProgress = new MetroFramework.Controls.MetroProgressBar();
+            this.StatusLabel = new MetroFramework.Controls.MetroLabel();
+            this.UseSeperateCheckBox = new MetroFramework.Controls.MetroCheckBox();
+            this.SuspendLayout();
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InstallButton.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.InstallButton.Location = new System.Drawing.Point(83, 140);
+            this.InstallButton.Name = "InstallButton ";
+            this.InstallButton.Size = new System.Drawing.Size(126, 54);
+            this.InstallButton.TabIndex = 2;
+            this.InstallButton.Text = "Install";
+            this.InstallButton.UseSelectable = true;
+            this.InstallButton.UseVisualStyleBackColor = true;
+            // 
+            // InstallDirectory
+            // 
+            this.InstallDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InstallDirectory.Lines = new string[0];
+            this.InstallDirectory.Location = new System.Drawing.Point(23, 63);
+            this.InstallDirectory.MaxLength = 32767;
+            this.InstallDirectory.Name = "InstallDirectory";
+            this.InstallDirectory.PasswordChar = '\0';
+            this.InstallDirectory.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.InstallDirectory.SelectedText = "";
+            this.InstallDirectory.Size = new System.Drawing.Size(226, 20);
+            this.InstallDirectory.TabIndex = 3;
+            this.InstallDirectory.UseSelectable = true;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowseButton.Location = new System.Drawing.Point(255, 63);
+            this.BrowseButton.Name = "browseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(22, 20);
+            this.BrowseButton.TabIndex = 4;
+            this.BrowseButton.Text = "...";
+            this.BrowseButton.UseSelectable = true;
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            // 
+            // InstallProgress
+            // 
+            this.InstallProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InstallProgress.Location = new System.Drawing.Point(24, 110);
+            this.InstallProgress.Name = "InstallProgress";
+            this.InstallProgress.Size = new System.Drawing.Size(254, 23);
+            this.InstallProgress.TabIndex = 5;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.StatusLabel.Location = new System.Drawing.Point(6, 210);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(89, 19);
+            this.StatusLabel.TabIndex = 6;
+            this.StatusLabel.Text = "Status: Ready.";
+            // 
+            // UseSeperateCheckBox
+            // 
+            this.UseSeperateCheckBox.AutoSize = true;
+            this.UseSeperateCheckBox.Checked = true;
+            this.UseSeperateCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.UseSeperateCheckBox.Location = new System.Drawing.Point(23, 87);
+            this.UseSeperateCheckBox.Name = "UseSeperateCheckBox";
+            this.UseSeperateCheckBox.Size = new System.Drawing.Size(200, 15);
+            this.UseSeperateCheckBox.TabIndex = 7;
+            this.UseSeperateCheckBox.Text = "Use seperate installation directory";
+            this.UseSeperateCheckBox.UseSelectable = true;
+            this.UseSeperateCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // InstallerForm
+            // 
+            this.ApplyImageInvert = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(300, 240);
+            this.Controls.Add(this.UseSeperateCheckBox);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.InstallProgress);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.InstallDirectory);
+            this.Controls.Add(this.InstallButton);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 240);
+            this.MinimumSize = new System.Drawing.Size(300, 210);
+            this.Name = "InstallerForm";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.Text = "Rocket Installer";
+            this.Load += new System.EventHandler(this.InstallerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+         private MetroButton InstallButton;
+         private MetroButton BrowseButton;
+         private MetroTextBox InstallDirectory;
+         private MetroProgressBar InstallProgress;
+         private MetroLabel StatusLabel;
+         private MetroCheckBox UseSeperateCheckBox;
+    }
+}
+
---
+++ RocketInstaller/InstallerForm.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using ICSharpCode.SharpZipLib.Zip;
+using MetroFramework;
+using MetroFramework.Forms;
+using Microsoft.Win32;
+
+namespace RocketInstaller
+{
+    public partial class InstallerForm : MetroForm
+    {
+        private static readonly char Sp = Path.DirectorySeparatorChar;
+        private const String API_KEY = "18682849-FBC3-4298-A7B6-1E4B71A60633";
+        public InstallerForm()
+        {
+            //Todo: add beta option
+            //Todo: make excluding of "Servers" directory optional
+
+            InitializeComponent();
+            BrowseButton.Click += BrowseButton_Click;
+            InstallButton.Click += InstallButton_Click;
+        }
+
+        private void InstallerForm_Load(object sender, EventArgs e)
+        {
+            String steamPath = GetSteamDirectory();
+            if (steamPath != null)
+            {
+                String unturnedDir = AppendDirectory(steamPath, "SteamApps", "common", "Unturned");
+                if (Directory.Exists(unturnedDir) && File.Exists(AppendDirectory(unturnedDir, "Unturned.exe")))
+                {
+                    InstallDirectory.Text = unturnedDir;
+                    StatusSuccess("Unturned found");
+                    return;
+                }
+                StatusError("Unturned not found");
+                return;
+            }
+            StatusError("Steam directory not found");
+        }
+
+        private static String AppendDirectory(String path, params String[] dirs)
+        {
+            if (path == null)
+            {
+                path = "";
+            }
+
+            if (dirs == null || !dirs.Any())
+            {
+                return path;
+            }
+
+            foreach (String s in dirs)
+            {
+                if (!path.EndsWith(Sp.ToString()))
+                {
+                    path += Sp;
+                }
+                path += s;
+            }
+
+            return path;
+        }
+
+        private static String GetSteamDirectory()
+        {
+            if (IsLinux())
+            {
+                if (File.Exists("~/.steam"))
+                {
+                    return "~/.steam";
+                }
+                if (File.Exists("~/.local/share/Steam"))
+                {
+                    return "~/.local/share/Steam";
+                }
+                return null;
+            }
+
+            //Windows
+            RegistryKey regKey = Registry.CurrentUser;
+            regKey = regKey.OpenSubKey(@"Software\Valve\Steam");
+
+            if (regKey == null || regKey.GetValue("SteamPath") == null) return null;
+
+            String dir=  regKey.GetValue("SteamPath").ToString();
+            return Directory.Exists(dir) ? dir.Replace(@"/", Sp.ToString()) : null;
+        }
+
+        public static bool IsLinux()
+        {
+            int p = (int)Environment.OSVersion.Platform;
+            return (p == 4) || (p == 6) || (p == 128);
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (GetSteamDirectory() != null)
+            {
+                String commonDir = AppendDirectory(GetSteamDirectory(), "SteamApps", "common");
+                fileDialog.InitialDirectory = Directory.Exists(commonDir) ? commonDir : GetSteamDirectory();
+            }
+
+            fileDialog.Filter = "Unturned (Unturned.exe)|Unturned.exe";
+            fileDialog.FilterIndex = 1;
+            fileDialog.RestoreDirectory = true;
+            if (fileDialog.ShowDialog() != DialogResult.OK) return;
+            String file = fileDialog.FileName;
+            if (!File.Exists(file)) return;
+            InstallDirectory.Text = Directory.GetParent(file).FullName;
+        }
+
+        private void InstallButton_Click(object sender, EventArgs e)
+        {
+            InstallButton.Enabled = false;
+            BrowseButton.Enabled = false;
+            InstallDirectory.Enabled = false;
+            InstallProgress.Visible = true;
+            InstallProgress.ProgressBarStyle = ProgressBarStyle.Marquee;
+            InstallProgress.Value = 1;
+            UseSeperateCheckBox.Enabled = false;
+
+            if (!Directory.Exists(InstallDirectory.Text))
+            {
+                InstallationFailed("Directory doesn't exists");
+                return;
+            }
+
+
+            if (!IsDirectory(InstallDirectory.Text)) 
+            {
+                InstallationFailed("Given path is not a directory");
+                return;
+            }
+
+            if (!File.Exists(AppendDirectory(InstallDirectory.Text, "Unturned.exe")))
+            {
+                InstallationFailed("Unturned.exe not found");
+                return;
+            }
+
+            try
+            {
+                Install();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Unexpected exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                InstallationFailed("Installation failed");
+            }
+        }
+
+        private String _unturnedDir;
+        private void Install()
+        {
+            String installDir = InstallDirectory.Text;
+            if (installDir.EndsWith(Sp.ToString()))
+            {
+                installDir.Remove(installDir.Length - 1); //Remove last char
+            }
+
+            const string serverSuffix = " - Server";
+            if (UseSeperateCheckBox.Checked && !installDir.EndsWith(serverSuffix))
+            {
+                StatusNeutral("Copying files...");
+                String copyDir = installDir + serverSuffix;
+                CopyDirs(installDir, copyDir);
+                return;
+            }
+            _unturnedDir = installDir;
+            StartDownload();
+        }
+
+        private async void CopyDirs(String sourceDir, String targetDir)
+        {
+            if (!Directory.Exists(targetDir))
+            {
+                Directory.CreateDirectory(targetDir);
+            }
+
+            foreach (string dirPath in Directory.GetDirectories(sourceDir, "*", SearchOption.AllDirectories).Where(dirPath => Path.GetDirectoryName(dirPath) != "Servers"))
+            {
+                Directory.CreateDirectory(dirPath.Replace(sourceDir, targetDir));
+                foreach (string filename in Directory.EnumerateFiles(dirPath))
+                {
+                    using (FileStream sourceStream = File.Open(filename, FileMode.Open))
+                    {
+                        using (FileStream destinationStream = File.Create(filename.Replace(sourceDir, targetDir)))
+                        {
+                            StatusNeutral("Copying: " + Path.GetFileName(filename));
+                            await sourceStream.CopyToAsync(destinationStream);
+                        }
+                    }
+                }
+            }
+
+            foreach (string filename in Directory.EnumerateFiles(sourceDir))
+            {
+                using (FileStream sourceStream = File.Open(filename, FileMode.Open))
+                {
+                    using (FileStream destinationStream = File.Create(targetDir + filename.Substring(filename.LastIndexOf('\\'))))
+                    {
+                        StatusNeutral("Copying: " + Path.GetFileName(filename));
+                        await sourceStream.CopyToAsync(destinationStream);
+                    }
+                }
+            }
+            _unturnedDir = targetDir;
+            StartDownload();
+        }
+
+        private async void CopyFile(String source, String target)
+        {
+            using (FileStream sourceStream = File.Open(source, FileMode.Open))
+            {
+                using (FileStream destinationStream = File.Create(target))
+                {
+                    await sourceStream.CopyToAsync(destinationStream);
+                }
+            }
+        }
+
+        private static readonly String TmpDir = AppendDirectory(Path.GetTempPath(), "RocketInstaller"); 
+        private static readonly String RocketZipFile = AppendDirectory(TmpDir, "Rocket.zip");
+        private static readonly String ExtractedDir = AppendDirectory(TmpDir, "Rocket");
+
+        private void StartDownload()
+        {
+            StatusNeutral("Starting download");
+            if (!Directory.Exists(TmpDir))
+            {
+                Directory.CreateDirectory(TmpDir);
+            }
+            InstallProgress.ProgressBarStyle = ProgressBarStyle.Blocks;
+            InstallProgress.Value = 0;
+            WebClient client = new WebClient();
+            client.DownloadProgressChanged += DownloadProgressChanged;
+            client.DownloadFileCompleted += DownloadFileCompleted;
+            if (IsLinux())
+            {
+                client.DownloadFileAsync(new Uri("http://api.rocket.foundation/linux-beta/latest/" + API_KEY), RocketZipFile);
+                return;
+            }
+            client.DownloadFileAsync(new Uri("http://api.rocket.foundation/beta/latest/" + API_KEY), RocketZipFile);
+        }
+
+        private void DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            InstallProgress.ProgressBarStyle = ProgressBarStyle.Marquee;
+            StatusNeutral("Download complete, extracting...");
+            Decompress(RocketZipFile, ExtractedDir);
+            StatusNeutral("Installing files...");
+            String managedDir = AppendDirectory(_unturnedDir, "Unturned_Data", "Managed");
+            String[] files = Directory.GetFiles(ExtractedDir);
+            foreach (String file in files)
+            {
+                String fileName = Path.GetFileName(file);
+                if (fileName == null || !fileName.EndsWith(".dll")) continue;
+                StatusNeutral("Installing: " + fileName);
+                CopyFile(file, AppendDirectory(managedDir, fileName));
+            }
+            String extractedScriptsDir = AppendDirectory(ExtractedDir, "Scripts");
+            String untScriptsDir = AppendDirectory(managedDir, "Scripts");
+            if (Directory.Exists(extractedScriptsDir ))
+            {
+                if (!Directory.Exists(untScriptsDir))
+                {
+                    Directory.CreateDirectory(untScriptsDir);
+                }
+                files = Directory.GetFiles(extractedScriptsDir );
+                foreach (String file in files)
+                {
+                    String fileName = Path.GetFileName(file);
+                    if (fileName == null) continue;
+
+                    StatusNeutral("Installing script: " + fileName);
+                    CopyFile(file, AppendDirectory(untScriptsDir, fileName));
+                }
+            }
+
+            InstallProgress.ProgressBarStyle = ProgressBarStyle.Blocks;
+            InstallProgress.Value = 100;
+
+            StatusSuccess("Installation was successful");
+
+            DialogResult dialogResult = MessageBox.Show("Installation was successful. Open server directory?", "Installation successful",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Process.Start ("file:///" + _unturnedDir);
+            }
+        } 
+
+        private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+            StatusNeutral("Downloaded " + e.BytesReceived + " of " + e.TotalBytesToReceive + "(" +(int) percentage + "%)");
+            InstallProgress.Value = int.Parse(Math.Truncate(percentage).ToString(CultureInfo.CurrentCulture));
+        }
+
+        private static bool IsDirectory(String s)
+        {
+            FileAttributes attr = File.GetAttributes(s);
+            return (attr & FileAttributes.Directory) == FileAttributes.Directory;
+        }
+
+        public static void Decompress(String file, String targetDir, String fileFilter = null)
+        {
+            FastZip fastZip = new FastZip();
+            fastZip.ExtractZip(file, targetDir, fileFilter);
+        }
+
+        private void InstallationFailed(String reason)
+        {
+            InstallButton.Enabled = true;
+            BrowseButton.Enabled = true;
+            InstallDirectory.Enabled = true;
+            InstallProgress.Visible = false;
+            StatusError("Failed: " + reason);
+        }
+
+        private void StatusSuccess(String text)
+        {
+            StatusLabel.Text = "Status: " + text;
+            StatusLabel.Style = MetroColorStyle.Green;
+        }
+
+        private void StatusError(String text)
+        {
+            StatusLabel.Text = "Status: " + text;
+            StatusLabel.Style = MetroColorStyle.Red;
+        }
+
+        private void StatusNeutral(string text)
+        {
+            StatusLabel.Text = "Status: " + text;
+            StatusLabel.Style = MetroColorStyle.Black;
+        }
+    }
+}
